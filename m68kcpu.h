4ced252b9c1a8dabba72749db447aa688aa99eae
shanshe <shanshe@gmail.com>
2021-01-23 00:55:47 +0100

    musashi typo

--- m68kcpu.h
+++ m68kcpu.h
@@ -677,7 +677,7 @@ extern jmp_buf m68ki_aerr_trap;
 /* Logging */
 #if M68K_LOG_ENABLE
 	#include <stdio.h>
-	extern FILE* M68K_LOG_FILEHANDLE
+	extern FILE* M68K_LOG_FILEHANDLE;
 	extern const char *const m68ki_cpu_names[];
 
 	#define M68K_DO_LOG(A) if(M68K_LOG_FILEHANDLE) fprintf A
