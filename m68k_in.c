33ff4010ffcdf0561360f8439570fc2ac62cf9b5
shanshe <shanshe@gmail.com>
2021-01-28 09:26:34 +0100

    ...

--- m68k_in.c
+++ m68k_in.c
@@ -134,7 +134,6 @@ M68KMAKE_TABLE_HEADER
 #include "m68kops.h"
 
 #define NUM_CPU_TYPES 5
-extern uint8_t realtime_disassembly;
 
 void  (*m68ki_instruction_jump_table[0x10000])(void); /* opcode handler jump table */
 unsigned char m68ki_cycles[NUM_CPU_TYPES][0x10000]; /* Cycles used by CPU type */
