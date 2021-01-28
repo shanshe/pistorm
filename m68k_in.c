e029b18a5ec314f98aa01d63faa89278fa1bca6c
shanshe <shanshe@gmail.com>
2021-01-28 10:07:07 +0100

    ...

--- m68k_in.c
+++ m68k_in.c
@@ -915,7 +915,7 @@ M68KMAKE_OP(1111, 0, ., .)
 M68KMAKE_OP(040fpu0, 32, ., .)
 {
 	printf("FPU 040fpu0 HAS_FPU=%d\n",!!HAS_FPU);
-	if(HAS_FPU)
+	if((CPU_TYPE_IS_030_PLUS(CPU_TYPE)) && (HAS_FPU))
 	{
 		m68040_fpu_op0();
 		return;
@@ -927,7 +927,7 @@ M68KMAKE_OP(040fpu0, 32, ., .)
 M68KMAKE_OP(040fpu1, 32, ., .)
 {
 	printf("FPU 040fpu1 HAS_FPU=%d\n",!!HAS_FPU);
-	if(HAS_FPU)
+	if((CPU_TYPE_IS_030_PLUS(CPU_TYPE)) && (HAS_FPU))
 	{
 		m68040_fpu_op1();
 		return;
