c26885d134b6666d5e7dd4f6a85eb6ca4f1e2d39
shanshe <shanshe@gmail.com>
2021-01-30 18:25:45 +0100

    debug mmu ...

--- m68kmmu.h
+++ m68kmmu.h
@@ -9,8 +9,9 @@
 
 // MMU status register bit definitions
 
-#if 0
-#define MMULOG printf
+#if 1
+#define MMULOG(...) MMULOG2((...))
+#define MMULOG2(A) if(realtime_disassembly){printf(A);}
 #else
 #define MMULOG(...)
 #endif
