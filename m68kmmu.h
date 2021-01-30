93d8da746c0e89882bcf4f3174e37c1f5d903b24
shanshe <shanshe@gmail.com>
2021-01-30 22:23:55 +0100

    enable address error exception

--- m68kmmu.h
+++ m68kmmu.h
@@ -127,6 +127,7 @@ void pmmu_set_buserror(uint32 addr_in)
 		m68ki_cpu.mmu_tmp_buserror_rw = m68ki_cpu.mmu_tmp_rw;
 		m68ki_cpu.mmu_tmp_buserror_fc = m68ki_cpu.mmu_tmp_fc;
 		m68ki_cpu.mmu_tmp_buserror_sz = m68ki_cpu.mmu_tmp_sz;
+		m68ki_exception_address_error(); // trigger an address error exception
 	}
 }
 
