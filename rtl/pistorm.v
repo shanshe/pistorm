97c3dbd716bc1783829a790ed9a482448ec0c6d5
shanshe <shanshe@gmail.com>
2021-03-01 16:00:33 +0100

    ...

--- rtl/pistorm.v
+++ rtl/pistorm.v
@@ -176,13 +176,13 @@ module pistorm(
 		ipl_reg <= 1'b0;
     end
     if (c7m_falling) begin
-      if (state == 2'd2) begin
+      if ((state == 2'd0)||(state == 2'd2)) begin
         ipl_1 <= ~M68K_IPL_n;
       end
 		ipl_2 <= ipl_1;
-      if (ipl_2 == ipl_1) begin
+//      if (ipl_2 == ipl_1) begin
         ipl <= ipl_2;
-      end
+//      end
     end
 
     if (ipl != last_ipl) begin
